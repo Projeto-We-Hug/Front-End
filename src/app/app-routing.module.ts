0d774ed60b70f59eb7fd94ad955cdcc3a0b3d28c
Mariana de Cássia Medrano <maremedrano@yahoo.com>
2022-01-21 00:26:05 -0300

    Estilização

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { EntrarComponent } from './entrar/entrar.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { SobreComponent } from './sobre/sobre.component';
 
 const routes: Routes = [
-
+  {path: '', redirectTo: 'inicio', pathMatch: 'full'},
+  {path: 'inicio', component: InicioComponent},
+  {path: 'sobre', component: SobreComponent},
+  {path: 'entrar', component: EntrarComponent},
+  {path: 'cadastro', component: CadastrarComponent}
 
 ];
 
